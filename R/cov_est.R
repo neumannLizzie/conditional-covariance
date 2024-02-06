7ccc2d3cd4abbe63af38744fcd2c0d525344b24d
neumannLizzie <neumannl@hsu-hh.de>
2024-02-06 16:06:52 +0100

    minor update

--- R/cov_est.R
+++ R/cov_est.R
@@ -18,8 +18,10 @@
 #' continuous ARL control.
 #' @param mx Numeric Matrix. Head start value as integer multiple of \code{1/m}.
 #' Should be an element of \code{0:hm}.
-#' @return Returns a single value which is the Average Run Length.
-#' @details TODO: Equations how the CUSUMs are implemented
+#' @return Returns a matrix with ncol(x) rows and ncol(x)+1 columns. 
+#' The first row of the ncol(x)+1 column contains the sum of the kernel (sumK).
+#' The first ncol(x) colums x ncol(x) rows are the covariance matrix at value znew.
+#' @details TODO: Equations how the cond cov is implemented
 #' @author Lizzie Neumann
 #' @references Neumann et al. (2024).
 #'  Confounder-adjusted Covariances of System Outputs and Applications to Structural Health Monitoring.
@@ -29,22 +31,13 @@
 #'  Nonparametric Covariance Model. \emph{Statistica Sinica} \strong{20}, 469-–79.
 #' @examples
 #' \dontrun{
-#' A   <- 500
-#' mu0 <- 1
-#' m   <- 20
-#' km1 <- 1.3*m
-#' km2 <- 2.2*m
-#' CDF <- function(q) ppois(q, lambda=mu0)
-#' ## calibrate Bi-CUSUM
-#' res1 <- bicusum_discrete_crit(CDF, mu0, A, km1, km2, m)
-#' ## ARLs of single CUSUM
-#' C1 <- cusum_discrete_arl(CDF, km1, res1$hm1, m, gamma=res1$g1, rando=TRUE)
-#' C2 <- cusum_discrete_arl(CDF, km2, res1$hm2, m, gamma=res1$g2, rando=TRUE)
-#' ## ARL of Bi-CUSUM with randomization
-#' CC <- bicusum_discrete_arl(CDF, km1, km2, res1$hm1, res1$hm2, m, rando=TRUE, g1=res1$g1, g2=res1$g2)
-#' cbind(round(cbind(C1, C2, CC), 3))
-#' ## ARL of Bi-CUSUM without randomization
-#' bicusum_discrete_arl(CDF, km1, km2, res1$hm1, res1$hm2, m)
+#' x <- cbind(c(1,3,5),c(3,2,6),c(4,1,5))
+#' z <- c(5.1,3.1,4.3)
+#' znew   <- 3.2
+#' h <- 1.3
+#' cest_old <- matrix(0,ncol=3,nrow=3)
+#' sumK_old <- 0
+#' mx <- c(3.2,2.2,1.1)
 #' }
 #' @export
 covest <- function(x, z, znew, h, cest_old, sumK_old, mx) {
@@ -61,8 +54,8 @@ covest <- function(x, z, znew, h, cest_old, sumK_old, mx) {
 }
 
 #' @name meanest
-#' @title Zero-state ARL of discrete CUSUM charts
-#' @description Compute the ARL of CUSUM.
+#' @title Nadaraya-Watson type kernel estimation of the conditional mean
+#' @description Estimation of the conditional mean.
 #' @param x Numreic Matrix An \R function of a discrete univariate Cumulative
 #'  Distribution Function taking a numeric first argument and returning a
 #'  numeric vector of the same length. For example, \code{\link{pbinom}} for
@@ -83,9 +76,12 @@ covest <- function(x, z, znew, h, cest_old, sumK_old, mx) {
 #' @return Returns a single value which is the Average Run Length.
 #' @details TODO: Equations how the CUSUMs are implemented
 #' @author Lizzie Neumann
-#' @references Brook and Evans (1972).
-#'  An approach to the probability distribution of CUSUM run length.
-#'  \emph{Biometrika}, \strong{59}, pp. 539--549
+#' @references Neumann et al. (2024).
+#'  Confounder-adjusted Covariances of System Outputs and Applications to Structural Health Monitoring.
+#'  \emph{PREPRINT}, \strong{XX}, pp. 1--26.
+#'
+#'  Yin et al. (2010).
+#'  Nonparametric Covariance Model. \emph{Statistica Sinica} \strong{20}, 469-–79.
 #' @examples
 #' \dontrun{
 #' A   <- 500
