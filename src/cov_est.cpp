3161b329c8228a17d4b6caf750e1ae7a548d0520
Philipp WIttenberg <pwitten@hsu-hh.de>
2024-02-07 10:26:36 +0100

    update namespace

--- src/cov_est.cpp
+++ src/cov_est.cpp
@@ -9,8 +9,8 @@ NumericMatrix cov_est(arma::mat x, NumericVector z, double znew, double h, arma:
   xc = x - mx;
   K = Rcpp::dnorm(z - znew, 0, h, false);
   for (i=0; i<n; i++) cest += K[i]*xc.row(i).t()*xc.row(i);
-  cest += sumK_old*cest_old;
-  cest /= sum(K)+sumK_old;
+  cest += sumK_old * cest_old;
+  cest /= sum(K) + sumK_old;
   sumK.row(0) = sum(K);
 return wrap(join_rows(cest, sumK));
 }
@@ -19,7 +19,7 @@ return wrap(join_rows(cest, sumK));
 NumericMatrix mean_est(arma::mat x, NumericVector z, double znew, double h, NumericVector mean_old, double sumK_old){
   arma::uword p = x.n_cols, n = x.n_rows, j, i;
   arma::colvec mx(p), K, sumK(p);
-  arma::mat mean1(p,2);
+  arma::mat mean1(p, 2);
 
   K = Rcpp::dnorm(z - znew, 0, h, false);
   for (i=0; i<n; i++) mx += K[i]*x.row(i).t();
